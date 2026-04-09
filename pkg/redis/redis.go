87d8a6220db96af5223da5d33c715b9403a54be0
john0504 <john2000@mail2000.com.tw>
2026-04-09 16:38:55 +0800

    fix redis

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -2,17 +2,30 @@ package redis
 
 import (
 	"context"
+	"log"
 	"os"
 
 	"github.com/redis/go-redis/v9"
 )
 
 var Client *redis.Client
+var Ctx = context.Background()
 
-func init() {
-	Client = redis.NewClient(&redis.Options{
-		Addr: os.Getenv("REDIS_URL"),
-	})
+func Init() {
+	url := os.Getenv("REDIS_URL")
 
-	Client.Ping(context.Background())
+	opt, err := redis.ParseURL(url)
+	if err != nil {
+		log.Fatal("Redis parse error:", err)
+	}
+
+	Client = redis.NewClient(opt)
+
+	// 測試連線
+	_, err = Client.Ping(Ctx).Result()
+	if err != nil {
+		log.Fatal("Redis connect error:", err)
+	}
+
+	log.Println("Redis connected")
 }
